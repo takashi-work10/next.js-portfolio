c4f8c6b35c2f821bb52978e28a9ad7ddaf54c710
takashi-work10 <takashi.yamada0606@gmail.com>
2025-01-28 11:53:10 +0900

    layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-import "./styles/all.css"
-import "./styles/blog.css"
-import "./styles/common.css" 
-import "./styles/contact.css"
-import "./styles/index.css"
-import "./styles/singleBlog.css"
-import Header from "./components/header"
-import Footer from "./components/footer"
+import "./styles/all.css";
+import "./styles/blog.css";
+import "./styles/common.css";
+import "./styles/contact.css";
+import "./styles/index.css";
+import "./styles/singleBlog.css";
+
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { ReactNode } from "react";
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -13,12 +15,14 @@ type RootLayoutProps = {
 
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
-    <html>
+    <html lang="en">
       <body>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
 };
 
-export default RootLayout
+export default RootLayout;
